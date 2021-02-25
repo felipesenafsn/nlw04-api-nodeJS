6320e334989daa6f5d16dc7e799091c2e585fa3c
Felipe Sena <felipesenafsn@gmail.com>
2021-02-24 22:15:18 -0300

    Semana de estudos NLW04

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { request, response } from 'express';
+import 'reflect-metadata';
+import "./database";
+import{router} from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => {
+    console.log("Servidor Rodando")
+});
