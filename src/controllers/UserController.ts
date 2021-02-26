8c1076f0108b72e4f1eccde9001bb665fbe5f858
Felipe Sena <felipesenafsn@gmail.com>
2021-02-25 21:34:02 -0300

    Criação de Novas Routes e Repositorios

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../respositories/UsersRepository";
 
 class UserController {
     async create(request: Request, response: Response) {
         const {name, email} = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         // select * from users where email = email    
         const userAlredyExists = await usersRepository.findOne({
@@ -30,4 +30,4 @@ class UserController {
     }
 }
 
-export { UserController }
+export { UserController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
+import { SurveysRepository } from "./respositories/SurveysRepository";
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
+router.post('/surveys', surveysController.create);
+router.get('/surveys', surveysController.show);
 router.post('/users', userController.create);
 
 export { router }
